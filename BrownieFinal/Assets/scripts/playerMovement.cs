63034d9684030ffd6229a0869db76e5581964990
Ochoa <737097@student.dusd.net>
2022-05-19 09:43:02 -0700

    Update playerMovement.cs

--- BrownieFinal/Assets/scripts/playerMovement.cs
+++ BrownieFinal/Assets/scripts/playerMovement.cs
@@ -6,6 +6,8 @@ public class playerMovement : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D body;
+    private float horizontal;
+    private float vertical;
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
@@ -13,6 +15,18 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal")* speed, body.velocity.y);
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        Vector2 move = new Vector2(horizontal, vertical);
+
+
+    }
+    void FixedUpdate()                
+    {
+        Vector2 position = body.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+        body.MovePosition(position);
+
     }
 }
